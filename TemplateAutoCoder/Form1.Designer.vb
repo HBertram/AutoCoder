e52e767ccbd8dda0c4eb4a698bc81569cac859df
HBertram <435832733@qq.com>
2018-11-30 13:24:40 +0800

    第一次

---
+++ TemplateAutoCoder/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RichTextBox1 = New TemplateAutoCoder.TemplateRichTextBox()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(117, 45)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(272, 158)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As TemplateRichTextBox
+End Class
---
+++ TemplateAutoCoder/TemplateRichTextBox.vb
@@ -0,0 +1,47 @@
+﻿Public Class TemplateRichTextBox
+    Inherits RichTextBox
+
+    Public Sub New()
+        WordWrap = False
+    End Sub
+
+    Protected Overrides Sub OnTextChanged(e As EventArgs)
+        MyBase.OnTextChanged(e)
+        Dim i As Integer = Me.SelectionStart
+        Dim s As String = Text
+        Text = ""
+        Dim c As Color = SelectionColor
+        Dim f As Font = SelectionFont
+        Dim start As Integer = s.IndexOf("##")
+        If start <> -1 Then
+        End If
+        Find("##*##", RichTextBoxFinds.NoHighlight)
+        SelectionFont = New Font("Verdana", 12, FontStyle.Bold)
+        SelectionColor = Color.Green
+        If SelectionLength > 0 Then
+            SelectedText = " " & SelectedText & " "
+        End If
+        
+        DeselectAll()
+        AppendText(s)
+        SelectionFont = f
+        SelectionColor = c
+        SelectionStart = i
+    End Sub
+
+
+    Private Sub ReplaceText()
+        Dim selectionStart As Integer = Me.SelectionStart
+        Dim oriText As String = Text
+        Dim i As Integer = 0
+        While True
+            SelectionColor = Me.ForeColor
+            Dim pos As Integer = oriText.IndexOf("##")
+            If pos = -1 Then
+
+            End If
+        End While
+        Me.SelectionStart = selectionStart
+    End Sub
+
+End Class
